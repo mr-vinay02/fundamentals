6a113a292b0e7ef241098d9b3020e240de842c20
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-17 12:59:41 +0530

    Using the implemented Array

---
+++ src/oopsimplementation/arrayimplementation/MyMainArray.java
@@ -0,0 +1,40 @@
+package oopsimplementation.arrayimplementation;
+
+public class MyMainArray {
+    public static void main(String[] args) {
+
+        MyArray array = new MyArray();
+
+        array.add(10);
+        array.add(20);
+        array.add(30);
+        array.add(50);
+        array.add("vnay");
+        array.add(true);
+        array.add(23423.34);
+        array.add(10);
+        array.add(20);
+        array.add(30);
+        array.add(50);
+        array.add("vnay");
+        array.add(true);
+        array.add(23423.34);
+        array.add(10);
+        array.add(20);
+        array.add(30);
+        array.add(50);
+        array.add("vnay");
+        array.add(true);
+        array.add(23423.34);
+        array.add(10);
+        array.add(20);
+        array.add(30);
+        array.add(50);
+        array.add("vnay");
+        array.add(true);
+        array.add(23423.34);
+
+        array.display();
+        System.out.println(array.get(2));
+    }
+}
