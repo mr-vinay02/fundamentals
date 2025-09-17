b120de37ef46287315939378e98a8918cbbb1e9c
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-17 16:59:40 +0530

    This is for the Part Time Employee

---
+++ src/oopsimplementation/employeesalarycalculation/PartTimeEmployee.java
@@ -0,0 +1,20 @@
+package oopsimplementation.employeesalarycalculation;
+
+public class PartTimeEmployee extends Employee{
+
+    private int hoursWorked;
+    private int salaryPerHour;
+
+    public PartTimeEmployee(int employeeId, String employeeName, int hoursWorked, int salaryPerHour) {
+        super(employeeId, employeeName);
+        this.hoursWorked = hoursWorked;
+        this.salaryPerHour = salaryPerHour;
+    }
+
+
+
+    @Override
+    double calculateSalary() {
+        return hoursWorked * salaryPerHour;
+    }
+}
