09b3ef6b6ad1d5a49ed62f46c94fec882f9ad617
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-17 13:15:24 +0530

    My Products

---
+++ src/oopsimplementation/inventorymanagementsystem/Products.java
@@ -0,0 +1,57 @@
+package oopsimplementation.inventorymanagementsystem;
+
+public class Products {
+    private int productid;
+    private String productName;
+    private double productprice;
+    private int productQuantity;
+
+    public Products(int productid, String productName, double productprice, int productQuantity) {git
+        this.productid = productid;
+        this.productName = productName;
+        this.productprice = productprice;
+        this.productQuantity = productQuantity;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getProductid() {
+        return productid;
+    }
+
+    public double getProductprice() {
+        return productprice;
+    }
+
+    public void setProductid(int productid) {
+        this.productid = productid;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductprice(double productprice) {
+        this.productprice = productprice;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Products{" +
+                "productid=" + productid +
+                ", productName='" + productName + '\'' +
+                ", productprice=" + productprice +
+                ", productQuantity=" + productQuantity +
+                '}';
+    }
+}
