65427af6ca5acd7235fce72ac098ef996ed74450
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-18 21:32:28 +0530

    This is a Used Main class for the Doubly Linked List

---
+++ src/datastructure/DoublyLinkedListImplementation.java
@@ -0,0 +1,26 @@
+package datastructure;
+
+public class DoublyLinkedListImplementation {
+    public static void main(String[] args) {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(50);
+        list.addFirst(05);
+        list.add(63,2);
+        list.add(10);
+        list.display();
+        System.out.println(list.delete(2));
+
+        list.deleteFirst();
+        list.deleteLast();
+        list.display();
+
+        list.printBackward();
+
+        list.
+
+        System.out.println(list.checkPalindrome());
+    }
+
+}
