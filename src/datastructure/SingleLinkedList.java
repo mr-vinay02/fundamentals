40e8649c9bd0fcc878e5a6035929d2f9d42559fb
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-18 15:29:30 +0530

    This is a Single Linked List

---
+++ src/datastructure/SingleLinkedList.java
@@ -0,0 +1,17 @@
+package datastructure;
+
+public class SingleLinkedList {
+
+    // create a class called Node
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data = data;
+        }
+    }
+
+
+}
