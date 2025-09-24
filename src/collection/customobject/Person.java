e27c00fee824f7ee24260411ae88ed8a2ebf8b9e
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-24 15:26:08 +0530

    This is a Person :Based on Name

---
+++ src/collection/customobject/Person.java
@@ -0,0 +1,42 @@
+package collection.customobject;
+
+public class Person implements Comparable{
+    private String name;
+    private int age;
+
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object object) {
+        Person person = (Person)object;
+        return this.name.compareTo(person.getName());
+    }
+}
