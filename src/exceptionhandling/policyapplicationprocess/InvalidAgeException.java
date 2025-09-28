c75e3ec6ac0d636fa8b6c1be6c98a4a92925c1b4
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-28 12:24:49 +0530

    This is a InvalidAgeException

---
+++ src/exceptionhandling/policyapplicationprocess/InvalidAgeException.java
@@ -0,0 +1,9 @@
+package exceptionhandling.policyapplicationprocess;
+
+public class InvalidAgeException extends Exception {
+
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+
+}
