b791e4184f64d58ce3159cb1c544ae1b83b0e918
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-28 12:28:07 +0530

    This is a HealthIssueException

---
+++ src/exceptionhandling/policyapplicationprocess/HealthIssueException.java
@@ -0,0 +1,9 @@
+package exceptionhandling.policyapplicationprocess;
+
+public class HealthIssueException extends Exception{
+
+    public HealthIssueException(String message) {
+        super(message);
+    }
+
+}
