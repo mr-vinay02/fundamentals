f934ddfa5eb67b066ff6114dc3ee498e38a012cc
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-28 12:26:45 +0530

    This is a PoorDrivingRecordException

---
+++ src/exceptionhandling/policyapplicationprocess/PoorDrivingRecordException.java
@@ -0,0 +1,9 @@
+package exceptionhandling.policyapplicationprocess;
+
+public class PoorDrivingRecordException extends Exception{
+
+    public PoorDrivingRecordException(String message) {
+        super(message);
+    }
+
+}
