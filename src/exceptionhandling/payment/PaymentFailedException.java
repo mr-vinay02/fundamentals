4238a77f3fcbec52ccbb98b8b09cc831152df603
mr-vinay02 <vinayvini4630@gmail.com>
2025-09-29 12:34:25 +0530

    This is a PaymentFailedException

--- src/exceptionhandling/payment/PaymentFailedException.java
+++ src/exceptionhandling/payment/PaymentFailedException.java
@@ -1,6 +1,6 @@
 package exceptionhandling.payment;
 
-public class PaymentFailedException extends Exception {
+public class PaymentFailedException extends RuntimeException {
 
     public PaymentFailedException(String message) {
         super(message);
