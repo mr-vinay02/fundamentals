62c0ad4cf3f56c5b7a1d886625b92dfd95d5bac1
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-07 12:48:30 +0530

    This is a Masala Chai

---
+++ src/designpatterns/onlinefoodorderingsystem/MasalaChai.java
@@ -0,0 +1,7 @@
+package designpatterns.onlinefoodorderingsystem;
+
+public class MasalaChai extends Drinks {
+    public String getName() { return "Masala Chai"; }
+    public double getPrice() { return 1.2; }
+    public String getDescription() { return "Spiced tea"; }
+}
