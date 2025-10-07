31070063abac50bdd388257aa01743eb27358238
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-07 12:43:34 +0530

    This is a Veg Burger

---
+++ src/designpatterns/onlinefoodorderingsystem/VegBurger.java
@@ -0,0 +1,7 @@
+package designpatterns.onlinefoodorderingsystem;
+
+public class VegBurger extends Burger {
+    public String getName() { return "Veg Burger"; }
+    public double getPrice() { return 5.0; }
+    public String getDescription() { return "Patty with veggies"; }
+}
