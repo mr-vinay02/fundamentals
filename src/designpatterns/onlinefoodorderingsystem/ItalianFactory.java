eaae2e1f1ed65a154edd05fc85229044a464f31a
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-07 14:13:42 +0530

    This is a Italian Factory

---
+++ src/designpatterns/onlinefoodorderingsystem/ItalianFactory.java
@@ -0,0 +1,18 @@
+package designpatterns.onlinefoodorderingsystem;
+
+public class ItalianFactory implements CuisineFactory{
+    @Override
+    public Pizza createPizza(String variant) {
+        return new MargheritaPizza();
+    }
+
+    @Override
+    public Burger createBurger(String variant) {
+        return new VegBurger();
+    }
+
+    @Override
+    public Drinks createDrink(String variant) {
+        return new Coke();
+    }
+}
