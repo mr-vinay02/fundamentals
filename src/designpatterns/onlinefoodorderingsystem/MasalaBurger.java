39276b2dfbfd1414c527b0ba57190bd673a08106
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-07 12:45:23 +0530

    This is a Masala  Burger

---
+++ src/designpatterns/onlinefoodorderingsystem/MasalaBurger.java
@@ -0,0 +1,7 @@
+package designpatterns.onlinefoodorderingsystem;
+
+public class MasalaBurger extends Burger {
+    public String getName() { return "Masala Burger"; }
+    public double getPrice() { return 6.0; }
+    public String getDescription() { return "Spiced Indian burger"; }
+}
