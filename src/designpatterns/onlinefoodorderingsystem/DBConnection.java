40da8cb5e70233b82363dd0457df18bef4437b85
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-07 12:11:46 +0530

    This is a DB Connection : singleton class

---
+++ src/designpatterns/onlinefoodorderingsystem/DBConnection.java
@@ -0,0 +1,20 @@
+package designpatterns.onlinefoodorderingsystem;
+
+
+// singleton class
+public class DBConnection {
+
+    private DBConnection(){
+        System.out.println("DB connection is established....");
+    }
+
+    private DBConnection instance;
+
+    public DBConnection getInstance(){
+        if(instance == null)
+            instance = new DBConnection();
+        return instance;
+    }
+
+
+}
