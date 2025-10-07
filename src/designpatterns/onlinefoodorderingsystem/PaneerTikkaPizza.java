acccae32b6b2dd8654226db7cbc06f2271217d9a
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-07 12:41:38 +0530

    This is a Panner Tikka Pizza

---
+++ src/designpatterns/onlinefoodorderingsystem/PaneerTikkaPizza.java
@@ -0,0 +1,7 @@
+package designpatterns.onlinefoodorderingsystem;
+
+public class PaneerTikkaPizza  extends Pizza {
+    public String getName() { return "Paneer Tikka Pizza"; }
+    public double getPrice() { return 10.0; }
+    public String getDescription() { return "Indian spiced paneer topping"; }
+}
