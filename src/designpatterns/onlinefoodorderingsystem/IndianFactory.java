c6fe228743db33cc2abdecd921b4ac54edf4df46
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-07 14:18:33 +0530

    This is a Indian Factory

---
+++ src/designpatterns/onlinefoodorderingsystem/IndianFactory.java
@@ -0,0 +1,18 @@
+package designpatterns.onlinefoodorderingsystem;
+
+public class IndianFactory implements CuisineFactory{
+    @Override
+    public Pizza createPizza(String variant) {
+        return new PaneerTikkaPizza();
+    }
+
+    @Override
+    public Burger createBurger(String variant) {
+        return new MasalaBurger();
+    }
+
+    @Override
+    public Drinks createDrink(String variant) {
+        return new MasalaChai();
+    }
+}
