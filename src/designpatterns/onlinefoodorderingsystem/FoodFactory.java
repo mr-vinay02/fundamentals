fbc550a528a53715a3f9d9d253be536be3eae94e
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-07 14:05:17 +0530

    This is a Food Factory : Factory Method

---
+++ src/designpatterns/onlinefoodorderingsystem/FoodFactory.java
@@ -0,0 +1,29 @@
+package designpatterns.onlinefoodorderingsystem;
+
+public class FoodFactory {
+
+    public static FoodItem create (String category , String variant){
+
+        switch (category){
+
+            case "Pizza" :
+                if(variant.equalsIgnoreCase("margherita"))
+                    return new MargheritaPizza();
+                else
+                    return new PaneerTikkaPizza();
+
+            case "Burger":
+                if(variant.equalsIgnoreCase("masalaburger"))
+                    return new MasalaBurger();
+                else
+                    return new VegBurger();
+
+            case "Drinks":
+                if(variant.equalsIgnoreCase("masalaChai"))
+                    return new MasalaChai();
+                else
+                    return new Coke();
+        }
+        throw new IllegalArgumentException("Unknown food item: " + category + " / " + variant);
+    }
+}
