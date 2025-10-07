28cd37d2fe321cd2e105043be6279c9b5eb61bdd
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-07 12:46:46 +0530

    This is a Coke

---
+++ src/designpatterns/onlinefoodorderingsystem/Coke.java
@@ -0,0 +1,7 @@
+package designpatterns.onlinefoodorderingsystem;
+
+public class Coke extends Drinks {
+    public String getName() { return "Coke"; }
+    public double getPrice() { return 1.5; }
+    public String getDescription() { return "Soft drink"; }
+}
