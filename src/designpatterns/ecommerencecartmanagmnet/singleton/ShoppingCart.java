99ca28bf119b36a7bd3ea8a29d224b4e6243d9f6
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-08 13:04:50 +0530

    This is a Singleton Design Pattern

---
+++ src/designpatterns/ecommerencecartmanagmnet/singleton/ShoppingCart.java
@@ -0,0 +1,28 @@
+package designpatterns.ecommerencecartmanagmnet.singleton;
+
+import designpatterns.ecommerencecartmanagmnet.factory.Product;
+
+import java.util.*;
+
+public class ShoppingCart {
+
+    private static ShoppingCart instance;
+    private List<Product> products = new ArrayList<>();
+
+    public ShoppingCart() {
+    }
+
+    public static ShoppingCart getInstance() {
+        if (instance == null)
+            instance = new ShoppingCart();
+        return instance;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        System.out.println(product.getName() + " added to cart.");
+        StockNotifier.getInstance().notifyObservers(product.getName() + " stock updated!");
+    }
+
+    public List<Product> getProducts() { return products; }
+}
