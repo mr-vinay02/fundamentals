984002062bee3208068a3c91af6c2fce4134e009
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-09 12:08:56 +0530

    This is a Abstract Design Pattern

--- src/designpatterns/ecommerencecartmanagmnet/abstractfactory/RegionFactory.java
+++ src/designpatterns/ecommerencecartmanagmnet/abstractfactory/RegionFactory.java
@@ -1,4 +1,4 @@
-package designpatterns.ecommerencecartmanagmnet.AbstractFactory;
+package designpatterns.ecommerencecartmanagmnet.abstractfactory;
 
 import designpatterns.ecommerencecartmanagmnet.factory.Product;
 
