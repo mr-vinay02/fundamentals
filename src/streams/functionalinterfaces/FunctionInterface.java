b7cf9208cdebdade9662b0c1b2020a151e63346b
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-09 14:58:57 +0530

    This is a Function Interface

---
+++ src/streams/functionalinterfaces/FunctionInterface.java
@@ -0,0 +1,29 @@
+package streams.functionalinterfaces;
+
+import java.util.function.Function;
+
+public class FunctionInterface {
+    public static void main(String[] args) {
+
+        // Using lambda function
+        Function<Integer,Integer> square = n -> n * n;
+
+        System.out.println(square.apply(10));
+
+        // Using Anonymous Implementation
+        Function<String,String> capital = new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s.toUpperCase();
+            }
+        };
+
+        System.out.println(capital.apply("vinay"));
+
+        //with method reference
+        Function<String,Integer> make = Integer::parseInt;
+
+        System.out.println(make.apply("500"));
+
+    }
+}
