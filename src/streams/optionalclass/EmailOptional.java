68a6d0bb90613a83cbf1ae1229beda35e4944e84
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-10 15:39:35 +0530

    This is a Optional if email is present or default

---
+++ src/streams/optionalclass/EmailOptional.java
@@ -0,0 +1,17 @@
+package streams.optionalclass;
+
+import java.util.Optional;
+
+public class EmailOptional {
+    public static void main(String[] args) {
+
+        String email1 = "vinayvini4630@gmail.com";
+        String email2 = null;
+
+        String result = Optional.ofNullable(email1).orElse("default@gmail.com");
+        String result2 = Optional.ofNullable(email2).orElse("NULL");
+
+        System.out.println(result);
+        System.out.println(result2);
+    }
+}
