7e74faf63d8f130122b62f385da9623445e54eba
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-10 14:19:51 +0530

    This is tells about the converting Array into Streams

--- src/streams/convertingintostreams/ArrayToStream.java
+++ src/streams/convertingintostreams/ArrayToStream.java
@@ -1,12 +1,18 @@
 package streams.convertingintostreams;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class ArrayToStream {
     public static void main(String[] args) {
 
 
         int[] array = {10,20,30,40,50};
+
+
+        array = Stream.of(array)
+                .flatMapToInt(Arrays::stream)
+                .toArray();
 
           array  = Arrays.stream(array)
                 .toArray();
