c2840efbb086b54306e1e204ac40e49406e5ff2d
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-15 14:15:17 +0530

    This is a class : Json Object

---
+++ src/filehandling/jsonfile/JsonData.java
@@ -0,0 +1,15 @@
+package filehandling.jsonfile;
+
+public class JsonData {
+    String name ;
+    int age ;
+    String[] skills;
+
+    public JsonData(String name, int age, String[] skills) {
+        this.name = name;
+        this.age = age;
+        this.skills = skills;
+    }
+
+
+}
